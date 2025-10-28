9f4f9d30070453e5b92dbdd4726a9920c906ee18
HugndUIT <hungnd1112006@gmail.com>
2025-10-29 06:26:42 +0700

    first commit

---
+++ Code-NT106.Q12.2-Lab03_24520602-24520549-24520254/Lab03_Bai01/DashBoard.cs
@@ -0,0 +1,10 @@
+namespace Lab03_Bai01
+{
+    public partial class DashBoard : Form
+    {
+        public DashBoard()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Code-NT106.Q12.2-Lab03_24520602-24520549-24520254/Lab03_Bai06/Form1.cs
@@ -0,0 +1,10 @@
+namespace Lab03_Bai06
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
